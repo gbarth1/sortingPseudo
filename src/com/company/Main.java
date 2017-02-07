34187f6995559a3897e461523abc24cfaad4cf5d
gracebarth <grace.m.barth@gmail.com>
2017-02-06 23:43:18 -0500

    h

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,21 @@
 package com.company;
-import java.util.*;
 public class Main {
 
     public static void main(String[] args)
     {
-        /*ArrayList<Integer> gb = new ArrayList();
-        gb.add(1);
-        gb.add(8);
-        gb.add(24);
-        gb.add(6);
-
-        if(gb){*/
 
         int list[] = new int[]{1, 8, 24, 6};
 
-        for(int i=0; i<list.length;i++)
-            if(list[i]>1){
-                list[i]=list[0];
+        for(int j=0; j<list.length;j++) {
 
+            for (int i = 0; i < list.length; i++) {
+                if (list[j] > list[i + 1]) {
+                    list[j] = list[i];
+                }
             }
 
-            if()
+        }
+
+        System.out.print(list[0]);
     }
 }
